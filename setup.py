3828af237b71ebdb819382b58beae77ccca591f9
tenways <rdasmith01@icloud.com>
2023-03-12 15:15:09 +0000

    PyPi Prep

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='python-bitcoinrpc-async',
     version='1.0.0',
     description='Enhanced version of python-jsonrpc for use with Bitcoin, for async use.',
-    long_description=open('README.md').read(),
+    long_description="python-bitcoinrpc-async adds asyncronous functionality to the AuthServiceProxy. It includes asyncio compatible methods for making and handling requests, making it easy to integrate into your codebase. With python-bitcoin-rpc you can now take advantage of the benefits of asynchronous programming, using the modern async and await syntax.",
     author='Reuben S',
     maintainer='Reuben S',
     url='https://github.com/reuben-s/python-bitcoinrpc-async',
